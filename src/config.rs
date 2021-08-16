f70811a8ccf65e1f768b62b9d378af1eac23f7e6
Javier López Úbeda <jlopezcur@gmail.com>
2021-08-16 18:34:07 +0200

    feat: added xdg dirs for config file

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,10 @@
+extern crate xdg;
+
 pub fn read() -> toml::Value {
-    let config_text = std::fs::read_to_string("/home/jlopez/dev/launcher/config.toml").unwrap();
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("slings").unwrap();
+    let config_path = xdg_dirs
+        .place_config_file("config.toml")
+        .expect("Cannot create config file");
+    let config_text = std::fs::read_to_string(config_path).unwrap();
     config_text.parse::<toml::Value>().unwrap()
 }
